5ecda6b8a5831ae7bb9276a1a8832525e8c4bd9e
Marcos <socram174@hotmail.com>
2024-11-09 16:49:36 -0300

    added port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { execSync } from 'child_process';
 import express from 'express';
 
+
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
@@ -8,6 +10,6 @@ app.get('/', (req, res) => {
     res.send(result);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on http://localhost:3000');
 });
